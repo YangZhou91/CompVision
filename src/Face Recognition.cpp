52eafc770d107320bb5c1083d7c3dd2fe075630f
Yang Zhou <gavinzhou10c@gmail.com>
2015-04-09 04:06:09 -0400

    [Add] Init the project

---
+++ src/Face Recognition.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Face.cpp
+// Author      : Yang Zhou
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
