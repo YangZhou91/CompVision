19480724f99deafd0fc4074561761554cb2d144a
Yang Zhou <gavinzhou10c@gmail.com>
2015-04-09 15:51:10 -0400

    [Add] Add the image loading part

--- src/Face Recognition.cpp
+++ src/Face Recognition.cpp
@@ -7,9 +7,37 @@
 //============================================================================
 
 #include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+
+
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	// to store the input file names
+	char * filename = new char[100];
+
+	// to store the current input image
+	cv::Mat input;
+
+	// To store keypoints
+	vector<cv::KeyPoint> keypints;
+	// To store the SIFT descriptor of current image
+	cv::Mat descriptor;
+	// To store all descriptor
+	cv::Mat featureUnclustered;
+
+	cv::SiftDescriptorExtractor detector;
+
+	sprintf(filename, "/Users/Gavin/Desktop/CompVision/Training Images/1.jpg");
+	input = cv::imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+	cv::imshow("Test", input);
+	cv::waitKey(0);
+
+	cout << "The end of the program" << endl; // prints !!!Hello World!!!
 	return 0;
 }
