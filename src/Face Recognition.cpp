87e11755798337bb32638d336f514668dd41163d
Yang Zhou <gavinzhou10c@gmail.com>
2015-04-10 18:57:33 -0400

    [Add] Add the block division algorithm of LBP

--- src/Face Recognition.cpp
+++ src/Face Recognition.cpp
@@ -14,65 +14,24 @@
 #include <opencv2/nonfree/features2d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+// import for LBP library
+#include "lbp.hpp"
+#include "histogram.hpp"
 
 using namespace std;
 using namespace cv;
 
 void drawBoundingBox();
+void drawSiftFeatures();
+void drawLBPFeatures();
+
 
 int main() {
 
 	// to store the input file names
-	char * filename = new char[100];
-
-	// to store the current input image
-	Mat input;
-
-	// To store keypoints
-	vector<KeyPoint> keypoints;
-	// To store the SIFT descriptor of current image
-	cv::Mat descriptor;
-	// To store all descriptor
-	cv::Mat featureUnclustered;
-	cv::SiftDescriptorExtractor detector;
-
-	// Construct image name
-	sprintf(filename, "/Users/Gavin/Desktop/CompVision/Training Images/1.jpg");
-	input = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
-
-	// detect feature points
-	detector.detect(input, keypoints);
-	detector.compute(input, keypoints, descriptor);
-
-	featureUnclustered.push_back(descriptor);
-
-	int dictionarySize = 200;
-
-	cv::TermCriteria tc(CV_TERMCRIT_ITER, 100, 0.001);
-
-	// number of retries
-	int retries = 1;
-	// K means center initialization
-	int flag = cv::KMEANS_PP_CENTERS;
-
-	// Create the BoW trainer
-	cv::BOWKMeansTrainer bowTrainer(dictionarySize, tc, retries, flag);
-	// Cluster feature vectors
-	cv::Mat dictonary = bowTrainer.cluster(featureUnclustered);
-
-	// store the vocabulary
-	cv::FileStorage fs("dictionary.yml", cv::FileStorage::WRITE);
-	fs << "vocabulart" << dictonary;
-	fs.release();
-//	imshow("Test", input);
-
-	Mat output;
-	drawKeypoints(input, keypoints, output);
-	imshow("Output", output);
-	waitKey(0);
-
+//	drawSiftFeatures();
 //	drawBoundingBox();
-
+	drawLBPFeatures();
 	cout << "The end of the program" << endl; // prints !!!Hello World!!!
 	return 0;
 }
@@ -134,4 +93,74 @@ void drawBoundingBox(){
 	  namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
 	  imshow( "Contours", drawing );
 	  waitKey(0);
+}
+
+void drawSiftFeatures() {
+	// to store the input file names
+	char* filename = new char[100];
+	// to store the current input image
+	Mat input;
+	// To store keypoints
+	vector<KeyPoint> keypoints;
+	// To store the SIFT descriptor of current image
+	cv::Mat descriptor;
+	// To store all descriptor
+	cv::Mat featureUnclustered;
+	cv::SiftDescriptorExtractor detector;
+	// Construct image name
+	sprintf(filename, "/Users/Gavin/Desktop/CompVision/Training Images/1.jpg");
+	input = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+	// detect feature points
+	detector.detect(input, keypoints);
+	detector.compute(input, keypoints, descriptor);
+	featureUnclustered.push_back(descriptor);
+	int dictionarySize = 200;
+	cv::TermCriteria tc(CV_TERMCRIT_ITER, 100, 0.001);
+	// number of retries
+	int retries = 1;
+	// K means center initialization
+	int flag = cv::KMEANS_PP_CENTERS;
+	// Create the BoW trainer
+	cv::BOWKMeansTrainer bowTrainer(dictionarySize, tc, retries, flag);
+	// Cluster feature vectors
+	cv::Mat dictonary = bowTrainer.cluster(featureUnclustered);
+	// store the vocabulary
+	cv::FileStorage fs("dictionary.yml", cv::FileStorage::WRITE);
+	fs << "vocabulart" << dictonary;
+	fs.release();
+	//	imshow("Test", input);
+	Mat output;
+	Mat output_resize;
+	drawKeypoints(input, keypoints, output);
+	resize(output, output_resize, Size(192,240));
+	imshow("SIFT_Feature", output_resize);
+	waitKey(0);
+}
+
+void drawLBPFeatures(){
+
+	Mat input;
+	// The size of small window
+	int N = 100;
+	vector<Mat> tiles;
+	char *filename = new char[100];
+	sprintf(filename, "/Users/Gavin/Desktop/CompVision/Training Images/1.jpg");
+	input = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+//	imshow("input", input);
+//	waitKey(0);
+
+	cout << "[drawLBPFeature] The size of input " << input.rows << " x " << input.cols << endl;
+	for(int x = 0; x < input.cols- N; x += N){
+
+		for(int y = 0; y < input.rows - N; y += N){
+			Mat tile = input(Rect(x, y, N, N));
+			tiles.push_back(tile);
+		}
+	}
+
+	imshow("tile", tiles.at(25));
+
+	cout << "[drawLBPFeatures] the size of tiles array: " << tiles.size() << endl;
+	waitKey(0);
+
 }
