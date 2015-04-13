cec65ffb47eb74870ff2762983c8f69c34fccacf
Gavin Zhou <gavinzhou10c@gmail.com>
2015-04-13 08:57:30 -0400

    [Add] initialize the loader part

--- src/Face Recognition.cpp
+++ src/Face Recognition.cpp
@@ -7,7 +7,7 @@
 //============================================================================
 
 // Get rid of errors for using sprintf
-#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
 
 
 #include <iostream>
@@ -33,9 +33,9 @@ struct MGHData
 	int angle;
 };
 
-void drawBoundingBox();
-void drawSiftFeatures();
-void drawLBPFeatures();
+
+void drawSiftFeatures(vector<MGHData> data);
+void drawLBPFeatures(vector<MGHData> data);
 
 int computeLbpCode(unsigned char seq[9]);
 int	*computeLbpHist(Mat &image, int *lbpHist);
@@ -48,17 +48,18 @@ bool MGHDataLoader(vector<MGHData> &trainingdataset, vector<MGHData> &testingdat
 int main() {
 
 	// to store the input file names
-//	drawSiftFeatures();
-//	drawBoundingBox();
 	
-	drawLBPFeatures();
 
-	cout << "The end of the program" << endl; // prints !!!Hello World!!!
-	return 0;
+	//drawLBPFeatures();
 
 	// To load data, use this
-	//vector<MGHData> trainingdata, testingdata,groupdata;
-	//MGHDataLoader(trainingdata, testingdata, groupdata, "Images/");
+	vector<MGHData> trainingdata, testingdata,groupdata;
+	MGHDataLoader(trainingdata, testingdata, groupdata, "Images/");
+
+	drawSiftFeatures(trainingdata);
+
+	cout << "The end of the program" << endl;
+	return 0;
 }
 
 
@@ -189,66 +190,7 @@ bool MGHDataLoader(vector<MGHData> &trainingdataset, vector<MGHData> &testingdat
 
 
 
-void drawBoundingBox(){
-
-	char * filename = new char[100];
-	sprintf(filename, "/Users/Gavin/Desktop/CompVision/Training Images/1.jpg");
-
-	Mat input;
-	Mat input_gray;
-	RNG rng(12345);
-
-	input = imread(filename);
-
-	if(!input.empty()){
-		// Convert RGB image to Gray Scale
-		cout << "[drawBoundingBox] Converting the original image to gray scale" << endl;
-		cvtColor(input, input_gray,CV_BGR2GRAY);
-		blur(input_gray, input_gray, Size(3,3));
-	}
-	else{
-		cout << "[drawBoundingBox] Could not read the image" << endl;
-	}
-
-	Mat threshold_output;
-	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-
-
- 	int thresh = 100;
-
-	// Detect edges using Threshold
-	threshold(input_gray, threshold_output, thresh, 255, THRESH_BINARY);
-	// Find contours
-	findContours(threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-	// Approximate contours to polygons
-	vector<vector<Point> > contours_poly(contours.size());
-	vector<Rect> boundRect(contours.size());
-	vector<Point2f> center(contours.size());
-	vector<float> radius(contours.size());
-
-	for(int i = 0; i < contours.size(); i++){
-		approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
-		boundRect[i] = boundingRect(Mat(contours_poly[i]));
-		minEnclosingCircle((Mat)contours_poly[i], center[i], radius[i]);
-	}
-
-	// Draw polygon contour + bonding rects
-	Mat drawing = Mat::zeros(threshold_output.size(), CV_8UC3);
-	for(int i =0; i < contours.size(); i++){
-		Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-		drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
-		rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-//		circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
-	}
-	/// Show in a window
-	  namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-	  imshow( "Contours", drawing );
-	  waitKey(0);
-}
-
-void drawSiftFeatures() {
+void drawSiftFeatures(vector<MGHData> data) {
 	// to store the input file names
 	char* filename = new char[100];
 	// to store the current input image
@@ -267,20 +209,7 @@ void drawSiftFeatures() {
 	detector.detect(input, keypoints);
 	detector.compute(input, keypoints, descriptor);
 	featureUnclustered.push_back(descriptor);
-	int dictionarySize = 200;
-	cv::TermCriteria tc(CV_TERMCRIT_ITER, 100, 0.001);
-	// number of retries
-	int retries = 1;
-	// K means center initialization
-	int flag = cv::KMEANS_PP_CENTERS;
-	// Create the BoW trainer
-	cv::BOWKMeansTrainer bowTrainer(dictionarySize, tc, retries, flag);
-	// Cluster feature vectors
-	cv::Mat dictonary = bowTrainer.cluster(featureUnclustered);
-	// store the vocabulary
-	cv::FileStorage fs("dictionary.yml", cv::FileStorage::WRITE);
-	fs << "vocabulart" << dictonary;
-	fs.release();
+	
 	//	imshow("Test", input);
 	Mat output;
 	Mat output_resize;
@@ -298,8 +227,7 @@ void drawLBPFeatures(){
 	input = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
 
 	Mat patch = input(Rect(300,200,60,60));
-	cout << "patch =" << endl;
-	cout << patch << endl;
+
 
 	int hist[256];
 	computeLbpHist(patch, hist);
@@ -316,7 +244,7 @@ void drawLBPFeatures(){
 		line(histImage, Point(bin_w*(i-1), hist_h - cvRound(hist[i-1])),
 				Point( bin_w*(i), hist_h - cvRound(hist[i-1]) ),
 				Scalar( 255, 0, 0), 2, 8, 0  );
-		cout << "[drawLBPFeature] hist " << i -1  << " "<< feature[i-1] << endl;
+		//cout << "[drawLBPFeature] hist " << i -1  << " "<< feature[i-1] << endl;
 	}
 
 	  /// Display
